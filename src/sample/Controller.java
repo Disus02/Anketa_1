734e51abd890b4cd86ff1981d4a7d4e336a738d8
Danil Stozhkov <lekseih2000@mail.ru>
2020-10-21 12:02:57 +0300

    Anketa

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -3,12 +3,18 @@ package sample;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.stage.DirectoryChooser;
+import javafx.stage.Stage;
 
+import java.awt.*;
+import java.io.File;
 import java.io.IOException;
 
 public class Controller {
     @FXML
-   private TextField name;
+    private TextField name;
     @FXML
     private TextField family;
     @FXML
@@ -19,26 +25,43 @@ public class Controller {
     private TextField pathFile;
     @FXML
     private Label textWindow;
-    String course= "";
+    String course = "";
     String pressradio = "";
 
 
     @FXML
     public void presscheck(ActionEvent event) {
-        course += " " + ((CheckBox)event.getSource()).getText();
+        course += " " + ((CheckBox) event.getSource()).getText();
 
     }
+
     @FXML
-    public void pressRadio(ActionEvent event){
-         pressradio = ((RadioButton)event.getSource()).getText();
+    public void pressRadio(ActionEvent event) {
+        pressradio = ((RadioButton) event.getSource()).getText();
 
     }
+
     Module module = new Module();
-    @FXML
- public void pressRegistr(ActionEvent event)throws IOException {
-        module.createFile(course,pressradio,name.getText(),family.getText(),otchestvo.getText(),nameFail.getText(),pathFile.getText());
-     textWindow.setText(name.getText() + "Вы зарегистрированы: ");
-     textWindow.getText();
-    }
 
+    @FXML
+    public void pressRegistr(ActionEvent event) throws IOException {
+        module.createFile(course, pressradio, name.getText(), family.getText(), otchestvo.getText(), nameFail.getText(), pathFile.getText());
+        textWindow.setText(name.getText() + "Вы зарегистрированы: ");
+        textWindow.getText();
+    }
+    @FXML
+    public void pressDir(ActionEvent event){
+       module.openDir(pathFile);
+
+    }
+    @FXML
+    public void pressFile(ActionEvent event) throws IOException {
+        module.openFile(pathFile.getText(),nameFail.getText(),pressradio);
+
+
+    }
 }
+
+
+
+
--- src/sample/Module.java
+++ src/sample/Module.java
@@ -1,19 +1,38 @@
 package sample;
 
 import javafx.scene.control.TextField;
+import javafx.stage.DirectoryChooser;
+import javafx.stage.Stage;
 
+import java.awt.*;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 
 public class Module {
     public void createFile(String Kurs, String pressradio,String name, String family, String otchestvo, String nameFail,String  pathFile) throws IOException {
-        File file = new File(pathFile +nameFail + "." + pressradio );
+        File file = new File(pathFile + "/" +nameFail + "." + pressradio );
         FileWriter fos = new FileWriter(file);
         file.createNewFile();
         fos.write("Уважаемый, "+ name +" "+ family+ " " + otchestvo +"\n" +"Вы записались на курс: " + Kurs);
         fos.close();
 
+
+    }
+    public void openFile(String path,String nameFile,String pas) throws IOException {
+        Desktop desktop = Desktop.getDesktop();
+        File file1=new File(path+"/"+ nameFile +"."+ pas);
+        if (!(file1==null)){
+            desktop.open(file1);
+        }
+    }
+    public void openDir(TextField path){
+        DirectoryChooser directoryChooser = new DirectoryChooser();
+        directoryChooser.setTitle("Выбрать папку");
+        File dir = directoryChooser.showDialog(new Stage());
+        if (!(dir==null)){
+            path.setText(dir.getAbsolutePath());
+        }
 
     }
 }
